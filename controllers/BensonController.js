a71ce253f33936ae0f155dc36580208f40eb4e28
x-zhq <x-zhq@163.com>
2024-07-15 20:15:41 -0400

    [SCRUM-72] backend code for login page

--- controllers/BensonController.js
+++ controllers/BensonController.js
@@ -1,10 +1,12 @@
 const {
     axios,
     path,
+    mysql2,
     fs
 } = require('../config/dependencies');
 const {
     "views-path-config": viewPaths,
+    "database-config": databaseConfig,
     "api-urls-config": APIs
 } = require('../config/config.json');
 
@@ -29,7 +31,7 @@ const getOrderListPageAction = (req, res) => {
     const orderListPage = viewPaths.orderList;
     const fpath = path.join(__dirname, orderListPage);
 
-    res.render(fpath, {});
+    res.render(fpath,  { username: req.session.username });
 };
 
 // access: /logout
@@ -39,20 +41,44 @@ const getLogOutAction = (req, res) => {
 }
 
 // operation: log in
-const postLoginAction = (req, res) =>  {
+const postLoginAction =  async (req, res) =>  {
     const name = req.body.name;
     const pass = req.body.password;
+    const users = await getUserByUserName(name);
+    console.log("-------users ------------: ");
+    console.log(users);
 
-    if(name == 'benson' & pass == '1234'){
+    if( pass == users.password){
         console.log('login success.');
-        req.session.username = 'benson';
+        req.session.username = name;
         req.session.userLoggedIn = true;
-            
         res.redirect('/order-list');
     }else{
         res.redirect('/login');
     }
 }
+
+
+const pool = mysql2.createPool({
+    host: databaseConfig['localhost'],
+    user: databaseConfig['username'],
+    password: databaseConfig['password'],
+    database: databaseConfig['database']
+}).promise();
+
+async function getUsers() {
+    const [rows] = await pool.query("SELECT * FROM user")
+    return rows;
+}
+
+async function getUserByUserName(uname) {
+    const [rows] = await pool.query(`
+    SELECT * 
+    FROM user
+    WHERE username = ?
+    `, [uname])
+    return rows[0]
+  }
 
 module.exports = {
     getLoginPageAction,
